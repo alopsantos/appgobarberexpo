4c4fb15c06fd26a924ce9fd6f0e0cdf0f58ed570
alopsantos <anderson_lops@hotmail.com>
2020-08-14 20:39:50 -0300

    Importando fontes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { View, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthRoutes from "./src/routes";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App: React.FC = () => (
+  <NavigationContainer >
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: "#312e38" }}>
+      <AuthRoutes />
     </View>
-  );
-}
+  </NavigationContainer>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ README.md
@@ -0,0 +1,20 @@
+## Configurações iniciais
+
+*expo init {nomedoprojeto}*
+
+*yarn add styled-components*
+*yarn add @types/styled-components -D*
+
+# Documentação React-navigation
+https://reactnavigation.org/docs/getting-started/
+
+
+*yarn add @react-navigation/native*
+
+## "Expo" 
+*expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view*
+
+## "React"
+*yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view*
+
+*yarn add @react-navigation/stack*
